3cea9854032fdc0f7a4ccfe71a7c10b8e904853d
Sandigupta <sandeep.2022ug1100@iiitranchi.ac.in>
2025-11-24 17:06:26 +0530

    Updated the config file

--- server/src/config/config.cjs
+++ server/src/config/config.cjs
@@ -3,26 +3,26 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        username: process.env.MYSQLUSER,
+        password: process.env.MYSQL_ROOT_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        host: process.env.RAILWAY_TCP_PROXY_DOMAIN,
+        port: process.env.RAILWAY_TCP_PROXY_PORT,
         dialect: 'mysql',
         logging: false
     },
     test: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
+        username: process.env.MYSQLUSER,
+        password: process.env.MYSQL_ROOT_PASSWORD,
         database: process.env.DB_NAME_TEST || 'code_copilot_test',
-        host: process.env.DB_HOST,
+        host: process.env.RAILWAY_TCP_PROXY_DOMAIN,
         dialect: 'mysql'
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username: process.env.MYSQLUSER,
+        password: process.env.MYSQL_ROOT_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        host: process.env.RAILWAY_TCP_PROXY_DOMAIN,
         dialect: 'mysql'
     }
 };
